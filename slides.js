c111de1b15f5847b8e21f5d9e8698c46c7beb3af
Pablo Bollansée <pablo.bollansee@gmail.com>
2014-10-16 22:02:43 +0200

    Moved all slide functionality to a namespace/object called Slides. Made some helper functions private in this namespace.

--- slides.js
+++ slides.js
@@ -1,72 +1,84 @@
-var SLIDE_CLOSED_HEIGHT = 65;
+// OO
+// http://stackoverflow.com/questions/881515/how-do-i-declare-a-namespace-in-javascript
+// Extra: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Introduction_to_Object-Oriented_JavaScript
+var Slides = new function() {
+	this.height = 85;
 
-function createSlides(slideIdArray, callback) {
-	var deferredList = new Array(slideIdArray.length);
+	// Load slides into divs with givben ID's.
+	// The slides have to be in html files named "slideId.html"
+	this.loadSlides = function(slideIdArray, callback) {
+		var deferredList = new Array(slideIdArray.length);
 
-	for (var i = 0; i < slideIdArray.length; i++) {
-		deferredList[i] =
-			$.Deferred(function (dfd) {
-				$("#"+slideIdArray[i]).load(slideIdArray[i]+".html", dfd.resolve);
-			}).promise();
+		for (var i = 0; i < slideIdArray.length; i++) {
+			deferredList[i] =
+				$.Deferred(function (dfd) {
+					$("#"+slideIdArray[i]).load(slideIdArray[i]+".html", dfd.resolve);
+				}).promise();
+		}
+
+		$.when.apply($, deferredList).then(callback);
 	}
 
-	$.when.apply($, deferredList).then(callback);
-}
+	// Initialize slides so they can be opened/closed.
+	// Everything with the class "slide" will get this functionality.
+	this.initializeSlides = function() {
+		var slideHeight = this.height;
+		$('.slides_wrapper .slide .slide_heading').click(function() {
+			var fullSlide = $(this).parent();
+			if(fullSlide.hasClass('current')) {
+				fullSlide.animate({"height": slideHeight}, {duration: "slow", queue: false, progress: animateTitle});
+				fullSlide.removeClass('current');
+			} else {
+				otherCurrent = $('.slides_wrapper .current');
+				otherCurrent.animate({"height": slideHeight}, {duration: "slow", queue: false, progress: animateTitle});
+				otherCurrent.children(".slide_title_relative_pos").animate({"opacity": 0}, {queue: false});
+				otherCurrent.removeClass('current');
 
-//add toggle function to slide_heading
-function initializeSlides() {
-	$('.slides_wrapper .slide .slide_heading').click(function() {
-		var fullSlide = $(this).parent();
-		if(fullSlide.hasClass('current')) {
-			fullSlide.animate({"height": SLIDE_CLOSED_HEIGHT}, {duration: "slow", queue: false, progress: animateTitle});
-			fullSlide.removeClass('current');
-		} else {
-			otherCurrent = $('.slides_wrapper .current');
-			otherCurrent.animate({"height": SLIDE_CLOSED_HEIGHT}, {duration: "slow", queue: false, progress: animateTitle});
-			otherCurrent.children(".slide_title_relative_pos").animate({"opacity": 0}, {queue: false});
-			otherCurrent.removeClass('current');
+				var cur_height = fullSlide.height();
+				var open_height = fullSlide.css("height", "auto").height();
+				fullSlide.height(cur_height).animate({"height": open_height}, {duration: "slow", queue: false, progress: animateTitle});
+				fullSlide.toggleClass('current');
+			}
+		});
 
-			var cur_height = fullSlide.height();
-			var open_height = fullSlide.css("height", "auto").height();
-			fullSlide.height(cur_height).animate({"height": open_height}, {duration: "slow", queue: false, progress: animateTitle});
-			fullSlide.toggleClass('current');
-		}
-	});
+		//reposition titles
+		$('.slides_wrapper .slide').each(function () {
+			repositionTitle($(this));
+		});
 
-	//reposition titles
-	$('.slides_wrapper .slide').each(function () {
-		repositionTitle($(this));
-	});
+		//show titles on mouse over
+		$('.slides_wrapper .slide .slide_heading').mouseenter(function () {
+			$(this).siblings(".slide_title_relative_pos").animate({"opacity": 1}, {queue: false});
+		});
+		$('.slides_wrapper .slide .slide_heading').mouseleave(function () {
+			if (!$(this).parent().hasClass('current')) {
+				$(this).siblings(".slide_title_relative_pos").animate({"opacity": 0}, {queue: false});
+			}
+		});
 
-	//show titles on mouse over
-	$('.slides_wrapper .slide .slide_heading').mouseenter(function () {
-		$(this).siblings(".slide_title_relative_pos").animate({"opacity": 1}, {queue: false});
-	});
-	$('.slides_wrapper .slide .slide_heading').mouseleave(function () {
-		if (!$(this).parent().hasClass('current')) {
-			$(this).siblings(".slide_title_relative_pos").animate({"opacity": 0}, {queue: false});
-		}
-	});
+		this.hideAllTitles();
+		this.closeAllSlides();
+	}
 
-	hideAllTitles();
-	closeAllSlides();
-}
+	// Instantly hide all titles
+	this.hideAllTitles = function() {
+		$(".slides_wrapper .slide .slide_title_relative_pos").css("opacity", 0);
+	}
 
-function repositionTitle(slide) {
-	var header = slide.children(".slide_heading");
-	var title = slide.children(".slide_title_relative_pos");
-	title.css("top", Math.min(-header.height() + slide.height(), 0));
-}
 
-//animate titles
-function animateTitle(animation, progress, remainingMs) {
-	repositionTitle($(animation.elem));
-}
+	// Instantly close all slides
+	this.closeAllSlides = function() {
+		$(".slides_wrapper .slide").css("height", this.height);
+	}
 
-function hideAllTitles() {
-	$(".slides_wrapper .slide .slide_title_relative_pos").css("opacity", 0);
-}
+	// Private functions, used to animate title
+	var repositionTitle = function(slide) {
+		var header = slide.children(".slide_heading");
+		var title = slide.children(".slide_title_relative_pos");
+		title.css("top", Math.min(-header.height() + slide.height(), 0));
+	}
+	var animateTitle = function(animation, progress, remainingMs) {
+		repositionTitle($(animation.elem));
+	}
 
-function closeAllSlides() {
-	$(".slides_wrapper .slide").css("height", SLIDE_CLOSED_HEIGHT);
 }
