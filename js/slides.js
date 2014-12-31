6fc0169e5c2deb4e5f5b81d0f4c0c4bd66e141fd
Pablo Bollansée <pablo.bollansee@gmail.com>
2014-12-31 14:32:01 +0100

    Made slides.js into a jquery plugin.

--- js/slides.js
+++ js/slides.js
@@ -3,73 +3,88 @@
 
 {% include detectmobilebrowser.js %}
 
-// OO
-// http://stackoverflow.com/questions/881515/how-do-i-declare-a-namespace-in-javascript
-// Extra: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Introduction_to_Object-Oriented_JavaScript
-var Slides = new function() {
-	this.height = 85;
+(function ( $ ) { //jquery wrapper
 
-	// Initialize slides so they can be opened/closed.
-	// Everything with the class "slide" will get this functionality.
-	this.initializeSlides = function() {
-		//add click function to all slide headings
-		$('.slide').each(function() {
+	$.fn.makeSlide = function(options) {
+		var defaults = {
+			height: 85,
+			fadeTitleOnMobile: false,
+			autoCloseOthers: false,
+			heightAnimOptions: {duration: 600, queue: false},
+			oppAnimOptions: {duration: 400, queue: false}
+		};
+		options = $.extend(defaults, options);
+
+		options.heightAnimOptions.progress = heightAnimCB;
+
+		// toggle open
+		this.each(function() {
 			var slide = $(this);
-
-			// toggle open
 			slide.find(".header").click(function() {
 				//if the slide is or open then close it
 				if(slide.hasClass('open')) {
-					slide.animate({"height": Slides.height}, {duration: "slow", queue: false, progress: animateTitle});
+					slide.animate({"height": options.height}, options.heightAnimOptions);
 					slide.removeClass('open');
 				}
 				else {
 					//close all open open slides
-					/*others = $('.open');
-					others.animate({"height": Slides.height}, {duration: "slow", queue: false, progress: animateTitle});
-					others.children(".title").animate({"opacity": 0}, {queue: false});
-					others.removeClass('open');*/
+					if (options.autoCloseOthers) {
+						others = $('.open');
+						others.animate({"height": options.height}, options.heightAnimOptions);
+						others.find(".header").children("span").animate({"opacity": 0}, {queue: false});
+						others.removeClass('open');
+					}
 
 					//open the clicked slide
 					var cur_height = slide.height();
 					var open_height = slide.css("height", "auto").height();
-					slide.height(cur_height).animate({"height": open_height}, {duration: "slow", queue: false, progress: animateTitle});
+					slide.height(cur_height).animate({"height": open_height}, options.heightAnimOptions);
 					$(this).children("span").animate({"opacity": 1}, {queue: false});
 					slide.toggleClass('open');
 				}
 			});
 
-			//close slide
-			slide.css("height", Slides.height);
-
-			//reposition title
-			Slides.repositionTitle(slide);
-
 			//show titles on mouse over, but not when on mobile browser
-			if (!jQuery.browser.mobile) {
+			if (!jQuery.browser.mobile || options.fadeTitleOnMobile) {
 				slide.find(".header")
 					.mouseleave(function () {
 						if (!slide.hasClass('open')) {
-							$(this).children("span").animate({"opacity": 0}, {queue: false});
+							$(this).children("span").animate({"opacity": 0}, options.oppAnimOptions);
 						}
 					})
 					.mouseenter(function () {
-						$(this).children("span").animate({"opacity": 1}, {queue: false});
+						$(this).children("span").animate({"opacity": 1}, options.oppAnimOptions);
 					})
 					.children("span").css("opacity", 0);
 			}
+			else {
+				slide.find(".header").children("span")
+					.css("opacity", 0)
+					.animate({"opacity": 1}, options.oppAnimOptions);
+			}
+
+			//when image loads slowly
+			reposSlideTitle(slide);
+			slide.find(".header").children("img").load( function() {
+				reposSlideTitle(slide);
+			});
 		});
+
+		//close slide
+		this.css("height", options.height);
+
+		return this;
 	}
 
-	// Private functions, used to animate title
-	//callback for progress of open animation
-	var animateTitle = function(animation, progress, remainingMs) {
-		Slides.repositionTitle($(animation.elem));
+	var heightAnimCB = function(animation, progress, remainingMs) {
+		reposSlideTitle($(animation.elem));
 	}
-	this.repositionTitle = function(slide) {
+
+	var reposSlideTitle = function(slide) {
 		var image = slide.find(".header img");
 		var title = slide.find(".header span");
 
 		title.css( "top", Math.min( 0, slide.height() - image.height() ) );
 	}
-}
+
+}( jQuery )); //jquery wrapper
