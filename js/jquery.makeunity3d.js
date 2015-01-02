96d823d610b85e4ce91836a8c216069f984d1a9c
Pablo Bollansée <pablo.bollansee@gmail.com>
2015-01-02 21:38:10 +0100

    Added cleaner way of adding unity webplayers to pages or slides. Also divided the main css file into parts and combine them with liquid.
    They can be set to only load on (for instance) a mouseover event.

---
+++ js/jquery.makeunity3d.js
@@ -0,0 +1,104 @@
+(function ($) { //jquery wrapper
+
+    var unity_missing_html =
+    '<div class="unity_missing" style="text-align: center; margin: auto; position: relative; top: 50%;">\
+        <a href="http:\/\/unity3d.com\/webplayer\/" title="Unity Web Player. Install now!">\
+            <img alt="Unity Web Player. Install now!" src="\/\/webplayer.unity3d.com\/installation\/getunity.png"\/>\
+        <\/a>\
+    <\/div>\
+    <div class="unity_broken" style="text-align: center; margin: auto; position: relative; top: 50%;">\
+        <a href="http:\/\/unity3d.com\/webplayer\/" title="Unity Web Player. Install now! Restart your browser after install.">\
+            <img alt="Unity Web Player. Install now! Restart your browser after install." src="http:\/\/webplayer.unity3d.com\/installation\/getunityrestart.png"\/>\
+        <\/a>\
+    <\/div>';
+
+    function loadUnityObjectJs(callback) {
+        var unityObjectUrl = "http://webplayer.unity3d.com/download_webplayer-3.x/3.0/uo/UnityObject2.js";
+        if (document.location.protocol == 'https:') {
+            unityObjectUrl = unityObjectUrl.replace("http://", "https://ssl-");
+        }
+        jQuery.ajax({
+            type: "GET",
+            url: unityObjectUrl,
+            success: callback,
+            dataType: "script",
+            cache: true
+        });
+    }
+
+    function initUnity(unityPlayer, options) {
+        var unityConfig = {
+            width: options.width,
+            height: options.height,
+            params: { enableDebugging:"0" }
+        };
+        var u = new UnityObject2(unityConfig);
+
+        unityPlayer.empty();
+        if (options.addMissingHtml) {
+            unityPlayer.append(unity_missing_html);
+        }
+
+        var $missingScreen = unityPlayer.find(".unity_missing");
+        var $brokenScreen = unityPlayer.find(".unity_broken");
+        $missingScreen.hide();
+        $brokenScreen.hide();
+
+        u.observeProgress(function (progress) {
+            switch(progress.pluginStatus) {
+                case "broken":
+                    $brokenScreen.find("a").click(function (e) {
+                        e.stopPropagation();
+                        e.preventDefault();
+                        u.installPlugin();
+                        return false;
+                    });
+                    $brokenScreen.show();
+                    break;
+                case "missing":
+                    $missingScreen.find("a").click(function (e) {
+                        e.stopPropagation();
+                        e.preventDefault();
+                        u.installPlugin();
+                        return false;
+                    });
+                    $missingScreen.show();
+                    break;
+                case "installed":
+                    $missingScreen.remove();
+                    break;
+                case "first":
+                    break;
+            }
+        });
+
+        var playerWebPath = unityPlayer.data("player-location");
+
+        u.initPlugin(unityPlayer.get(0), playerWebPath);
+    };
+
+    $.fn.makeUnity3D = function(options) {
+        var defaults = {
+            addMissingHtml: true,
+            activationEvent: null,
+            width: 800,
+            height: 450
+        };
+        options = $.extend(defaults, options);
+
+        var unityDivs = this;
+        var callback = function() {
+            if (options.activationEvent) {
+                unityDivs.one( options.activationEvent, function() { initUnity($(this), options); } );
+            }
+            else {
+                unityDivs.each( function() { initUnity($(this), options); } );
+            }
+        };
+
+        loadUnityObjectJs(callback);
+
+        return this;
+    }
+
+} (jQuery) ); //jquery wrapper
--- masterwars/index.md
+++ masterwars/index.md
@@ -3,68 +3,14 @@ layout: more_page
 title: Master Wars
 ---
 
-<style type="text/css">
+<script type="text/javascript" src="../js/jquery.makeunity3d.js"></script>
+<script type="text/javascript">
+	$(document).ready(function() {
+		$(".unityPlayer").makeUnity3D();
+	});
+</script>
 
-	<!--
-	body {
-		font-family: Helvetica, Verdana, Arial, sans-serif;
-		background-color: white;
-		color: black;
-		text-align: center;
-	}
-	a:link, a:visited {
-		color: #000;
-	}
-	a:active, a:hover {
-		color: #666;
-	}
-	p.header {
-		font-size: small;
-	}
-	p.header span {
-		font-weight: bold;
-	}
-	p.footer {
-		font-size: x-small;
-	}
-	div.broken,
-	div.missing {
-		margin: auto;
-		position: relative;
-		top: 50%;
-		width: 193px;
-	}
-	div.broken a,
-	div.missing a {
-		height: 63px;
-		position: relative;
-		top: -31px;
-	}
-	div.broken img,
-	div.missing img {
-		border-width: 0px;
-	}
-	div.broken {
-		display: none;
-	}
-	div#unityPlayer {
-		cursor: default;
-		height: 600px;
-		width: 800px;
-	}
-	-->
-
-</style>
-
-
-
-<div id="unityPlayer">
-	<div class="missing">
-		<a href="http://unity3d.com/webplayer/" title="Unity Web Player. Install now!">
-			<img alt="Unity Web Player. Install now!" src="http://webplayer.unity3d.com/installation/getunity.png" width="193" height="63" />
-		</a>
-	</div>
-</div>
+<div class="unityPlayer" data-player-location="//dl.dropboxusercontent.com/u/10448192/Master%20Wars%20Ball/Master%20Wars%20Ball.unity3d"></div>
 
 ## How To Play
 
@@ -83,68 +29,3 @@ To end your turn click the menu button, bottom right, and click the 'next turn' 
 ## Master Wars
 
 Master Wars is a litte project I started years ago. I first made a very simple 2D clone of Advance Wars with a friend as a school project. Later I decided to make my own turn-based strategy in 3D using Unity, still based on Advance Wars but no longer a clone. It used hexagons as tiles but it was still a flat playing field. Then I got an internship at Vanguard Games and I decided to rewrite the whole game since I learned a lot there. I then changed to world from a flat playing field, to the planets I have now.
-
-
-<script type='text/javascript' src='https://ssl-webplayer.unity3d.com/download_webplayer-3.x/3.0/uo/jquery.min.js'></script>
-
-<script type="text/javascript">
-
-	<!--
-	var unityObjectUrl = "http://webplayer.unity3d.com/download_webplayer-3.x/3.0/uo/UnityObject2.js";
-	if (document.location.protocol == 'https:')
-		unityObjectUrl = unityObjectUrl.replace("http://", "https://ssl-");
-	document.write('<script type="text\/javascript" src="' + unityObjectUrl + '"><\/script>');
-	-->
-
-</script>
-
-<script type="text/javascript">
-
-	<!--
-	var config = {
-		width: 800,
-		height: 600,
-		params: { enableDebugging:"0" }
-
-	};
-	config.params["disableContextMenu"] = true;
-	var u = new UnityObject2(config);
-
-	jQuery(function() {
-		var $missingScreen = jQuery("#unityPlayer").find(".missing");
-		var $brokenScreen = jQuery("#unityPlayer").find(".broken");
-		$missingScreen.hide();
-		$brokenScreen.hide();
-
-		u.observeProgress(function (progress) {
-			switch(progress.pluginStatus) {
-			case "broken":
-				$brokenScreen.find("a").click(function (e) {
-					e.stopPropagation();
-					e.preventDefault();
-					u.installPlugin();
-					return false;
-				});
-				$brokenScreen.show();
-				break;
-			case "missing":
-				$missingScreen.find("a").click(function (e) {
-					e.stopPropagation();
-					e.preventDefault();
-					u.installPlugin();
-					return false;
-				});
-				$missingScreen.show();
-				break;
-			case "installed":
-				$missingScreen.remove();
-				break;
-			case "first":
-				break;
-			}
-		});
-		u.initPlugin(jQuery("#unityPlayer")[0], "//dl.dropboxusercontent.com/u/10448192/Master%20Wars%20Ball/Master%20Wars%20Ball.unity3d");
-	});
-	-->
-
-</script>
